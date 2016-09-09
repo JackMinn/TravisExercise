12c56d51c475a03dcddddc47d30c2d4342e3d0ac
JackMinn <jackminnetian@gmail.com>
2016-09-09 15:16:16 -0400

    commit

---
+++ test/strategydemo/TestFlatRateDiscount.java
@@ -0,0 +1,16 @@
+package strategydemo;
+
+import strategydemo.Order;
+
+public class TestFlatRateDiscount {
+
+		public void testFlatRateDiscount1() {
+			Order order = new Order("John", 200.24f);
+			float discount = 10;
+//			FlatRateDiscountStrategy strategy = new FlatRate
+//					order.setStrategy(strategy);;
+//					assertEquals(190.24, order.getDiscountedPrice(),
+		}
+		
+
+}
